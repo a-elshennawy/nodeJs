6669e06ccb7521391f24240cd020b286fc1aacd4
shennawy <ahmedshennawy997@gmail.com>
2026-02-21 14:53:04 +0200

    integration

--- src/server.js
+++ src/server.js
@@ -1,15 +1,39 @@
 import express from "express";
+import moviesRoutes from "./routes/moviesRoutes.js";
+import { config } from "dotenv";
+import { connectDB, disconnectDB } from "./config/db.js";
+
+config();
+connectDB();
 
 const app = express();
 const PORT = 5001;
 
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello nigga!" });
-});
+app.use("/movies", moviesRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
 
-// GET, POST, PUT, DELETE
-// http://localhost:5001/
+// handle error cases
+process.on("unhandledRejection", (err) => {
+  console.error(err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", async (err) => {
+  console.error(err);
+  await disconnectDB();
+  process.exit(1);
+});
+
+process.on("SIGTERM", (err) => {
+  console.error(err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
+});
